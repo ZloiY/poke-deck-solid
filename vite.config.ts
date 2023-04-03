5fef3624023a4395c923a24bcd994ec1a760a9af
Maksim Bakhanovich <maksim.bakhanovich@protonmail.com>
2023-04-03 19:04:05 +0300

    chore: added vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import solid from "solid-start/vite";
 import solidSvg from "vite-plugin-solid-svg";
+import vercel from "solid-start-vercel";
 import alias from "@rollup/plugin-alias";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    solid(),
+    solid({ adapter: vercel({edge: false}) }),
     solidSvg({
       defaultAsComponent: true,
     }),
