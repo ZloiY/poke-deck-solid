2683a8c7a4f1f16b56fc4717ce16bb2892eaca62
Maksim Bakhanovich <maksim.bakhanovich@protonmail.com>
2023-04-05 21:37:55 +0300

    feat(strem): render to stream

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,9 @@
+import { Suspense } from "solid-js";
 import { Outlet, useRouteData } from "solid-start";
 import { createServerData$ } from "solid-start/server";
 import { useUser } from "~/actions/useUser";
 import { HeaderLayout } from "~/components/layouts/HeaderLayout";
+import { Spinner } from "~/components/Spinner";
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
@@ -13,7 +15,14 @@ export default function HomeLayout() {
   const user = useRouteData<typeof routeData>();
   return (
     <HeaderLayout title="PokeDeck Home" showFlip={true} user={user()}>
-      <Outlet/>
+      <Suspense
+        fallback={
+          <div class="h-full w-full flex justify-center items-center">
+            <Spinner className="h-56 w-56"/>
+          </div>
+        }>
+        <Outlet/>
+      </Suspense>
     </HeaderLayout>
   )
 };
